ef3727da6da063d04fb4bd384f9a2f788ca7ad17
Instance of Human <253295491+instance-of-human@users.noreply.github.com>
2026-01-07 07:55:05 +0100

    Checking serge and clip possibilities

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// JSON file with tasks list.
+    #[arg(short, long, value_name = "FILE")]
+    tasks_path: PathBuf,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+
+    println!("Value for config: {:#}", cli.tasks_path.display());
+
+    let json_data = fs::read_to_string("tasks.json").unwrap();
+    let task_list: Vec<String> = serde_json::from_str(&json_data).unwrap();
+    println!("{:?}", task_list);
 }
