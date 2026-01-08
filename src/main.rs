adbd1bd9b79812c563700babe270edee3cafeb26
Instance of Human <253295491+instance-of-human@users.noreply.github.com>
2026-01-08 08:01:02 +0100

    List, add, remove tasks template

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -10,15 +10,69 @@ struct Cli {
     #[arg(short, long, value_name = "FILE")]
     tasks_path: PathBuf,
 
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List all tasks.
+    List {},
+
+    /// Add new task to list.
+    Add {
+        /// Name of the task
+        #[arg(short, long)]
+        name: String,
+    },
+
+    /// Remove task from list.
+    Remove {
+        /// Name of the task
+        #[arg(short, long)]
+        name: String,
+    },
+
 }
 
 fn main() {
     let cli = Cli::parse();
 
+    let data: String = match fs::read_to_string(cli.tasks_path) {
+        Ok(data) => data,
+        Err(_) => {
+            println!("Could not read the file !");
+            return;
+        }
+    };
 
-    println!("Value for config: {:#}", cli.tasks_path.display());
+    let tasks: Vec<String> = match serde_json::from_str(&data){
+        Ok(data) => data,
+        Err(_) => {
+            println!("Could not parse the file !");
+            return;
+        }
+    };
 
-    let json_data = fs::read_to_string("tasks.json").unwrap();
-    let task_list: Vec<String> = serde_json::from_str(&json_data).unwrap();
-    println!("{:?}", task_list);
+    match &cli.command {
+        Some(Commands::List {}) => {
+            println!("Current tasks list: ");
+            for task in &tasks{
+                println!("- {task}");
+            }
+        }
+        Some(Commands::Add { name }) => {
+
+        }
+        Some(Commands::Remove { name }) => {
+
+        }
+        None => {
+            println!("Not action were selected!");
+        }
+    }
+
+
+
 }
